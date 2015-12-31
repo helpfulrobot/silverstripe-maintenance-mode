8dead3a90a147105b3afce4c83e2d4b075f3b851
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:23:58 +1300

    Converted to PSR-2

--- code/UtilityPage.php
+++ code/UtilityPage.php
@@ -7,157 +7,159 @@
  * @author Darren-Lee Joseph <darrenleejoseph@gmail.com>
  * @package maintenancemode
  */
-class UtilityPage extends ErrorPage {
+class UtilityPage extends ErrorPage
+{
 
-	private static $singular_name = 'Utility Page';
-	private static $plural_name = 'Utility Pages';
-	private static $description = 'Use this to create a Down for Maintenance, Under Construction or Coming Soon Page';
-	private static $icon = "maintenance-mode/images/tools-icon.png";
+    private static $singular_name = 'Utility Page';
+    private static $plural_name = 'Utility Pages';
+    private static $description = 'Use this to create a Down for Maintenance, Under Construction or Coming Soon Page';
+    private static $icon = "maintenance-mode/images/tools-icon.png";
 
-	private static $db = array(
-		'RenderingTemplate' => 'Varchar(64)'
-	);
+    private static $db = array(
+        'RenderingTemplate' => 'Varchar(64)'
+    );
 
-	private static $has_one = array();
+    private static $has_one = array();
 
-	private static $has_many = array();
+    private static $has_many = array();
 
-	private static $defaults = array(
-		'ErrorCode' => '503'
-	);
+    private static $defaults = array(
+        'ErrorCode' => '503'
+    );
 
-	/**
-	 * @param Member $member
-	 * @return boolean
-	 */
-	public function canCreate($member = null) {
-		// Only allow one of this Page type to be created in the CMS.
-		return !UtilityPage::get()->exists();
-	}
-
-
-	/**
-	 * Create default Utility Page setup
-	 * Ensures that there is always a 503 Utility page by checking if there's an
-	 * instance of ErrorPage with a 503 error code. If there is not,
-	 * one is created when the DB is built.
-	 */
-	public function requireDefaultRecords() {
-		parent::requireDefaultRecords();
-
-		// Skip creation of default records
-		if(!self::config()->create_default_pages) return;
-
-		// Ensure that an assets path exists before we do any error page creation
-		if(!file_exists(ASSETS_PATH)) {
-			mkdir(ASSETS_PATH);
-		}
-
-		$code = self::$defaults['ErrorCode'];
-		$pagePath = self::get_filepath_for_errorcode($code);
-		$page = UtilityPage::get()->first();
-		$pageExists = ($page && $page->exists());
-
-		//Only create a UtilityPage on dev/build if one does not already exist.
-		if(!$pageExists or !file_exists($pagePath)) {
-
-			if(!$pageExists) {
-				$page = UtilityPage::create(array(
-					'Title' => _t('MaintenanceMode.TITLE', 'Undergoing Scheduled Maintenance'),
-					'URLSegment' => _t('MaintenanceMode.URLSEGMENT', 'offline'),
-					'MenuTitle' => _t('MaintenanceMode.MENUTITLE', 'Utility Page'),
-					'Content' => _t('MaintenanceMode.CONTENT', '<h1>We&rsquo;ll be back soon!</h1>'
-						. '<p>Sorry for the inconvenience but '
-						. 'our site is currently down for scheduled maintenance. '
-						. 'If you need to you can always <a href="mailto:#">contact us</a>, '
-						. 'otherwise we&rsquo;ll be back online shortly!</p>'
-						. '<p>&mdash; The Team</p>'),
-					'ParentID' => 0,
-					'Status' => 'Published'
-				));
-				$page->write();
-				$page->publish('Stage', 'Live');
-			}
-
-			// Ensure a static error page is created from latest Utility Page content
-			$response = Director::test(Director::makeRelative($page->Link()));
-			$written = null;
-			if($fh = fopen($pagePath, 'w')) {
-				$written = fwrite($fh, $response->getBody());
-				fclose($fh);
-			}
-
-			if($written) {
-				DB::alteration_message(
-					sprintf('%s error Utility Page created', $code),
-					'created'
-				);
-			} else {
-				DB::alteration_message(
-					sprintf(
-						'%s error Utility page could not be created at %s. Please check permissions',
-						$code,
-						$pagePath
-					),
-					'error'
-				);
-			}
-
-		}
-	}
+    /**
+     * @param Member $member
+     * @return boolean
+     */
+    public function canCreate($member = null)
+    {
+        // Only allow one of this Page type to be created in the CMS.
+        return !UtilityPage::get()->exists();
+    }
 
 
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
+    /**
+     * Create default Utility Page setup
+     * Ensures that there is always a 503 Utility page by checking if there's an
+     * instance of ErrorPage with a 503 error code. If there is not,
+     * one is created when the DB is built.
+     */
+    public function requireDefaultRecords()
+    {
+        parent::requireDefaultRecords();
 
-		$fields->addFieldToTab('Root.Main',
+        // Skip creation of default records
+        if (!self::config()->create_default_pages) {
+            return;
+        }
 
-			$templateDropdownField = DropdownField::create(
-				'RenderingTemplate',
-				_t('MaintenanceMode.RENDERINGTEMPLATE', 'Template to render with'),
-				self::get_top_level_templates()
-			), 'Content'
+        // Ensure that an assets path exists before we do any error page creation
+        if (!file_exists(ASSETS_PATH)) {
+            mkdir(ASSETS_PATH);
+        }
 
-		);
+        $code = self::$defaults['ErrorCode'];
+        $pagePath = self::get_filepath_for_errorcode($code);
+        $page = UtilityPage::get()->first();
+        $pageExists = ($page && $page->exists());
 
-		$templateDropdownField->setEmptyString(_t('MaintenanceMode.DEFAULTTEMPLATE', '(Use default template)'));
+        //Only create a UtilityPage on dev/build if one does not already exist.
+        if (!$pageExists or !file_exists($pagePath)) {
+            if (!$pageExists) {
+                $page = UtilityPage::create(array(
+                    'Title' => _t('MaintenanceMode.TITLE', 'Undergoing Scheduled Maintenance'),
+                    'URLSegment' => _t('MaintenanceMode.URLSEGMENT', 'offline'),
+                    'MenuTitle' => _t('MaintenanceMode.MENUTITLE', 'Utility Page'),
+                    'Content' => _t('MaintenanceMode.CONTENT', '<h1>We&rsquo;ll be back soon!</h1>'
+                        . '<p>Sorry for the inconvenience but '
+                        . 'our site is currently down for scheduled maintenance. '
+                        . 'If you need to you can always <a href="mailto:#">contact us</a>, '
+                        . 'otherwise we&rsquo;ll be back online shortly!</p>'
+                        . '<p>&mdash; The Team</p>'),
+                    'ParentID' => 0,
+                    'Status' => 'Published'
+                ));
+                $page->write();
+                $page->publish('Stage', 'Live');
+            }
 
-		$this->extend('updateCMSFields', $fields);
-		return $fields;
-	}
+            // Ensure a static error page is created from latest Utility Page content
+            $response = Director::test(Director::makeRelative($page->Link()));
+            $written = null;
+            if ($fh = fopen($pagePath, 'w')) {
+                $written = fwrite($fh, $response->getBody());
+                fclose($fh);
+            }
 
-	/**
-	 * This function returns an array of top-level theme templates
-	 * @return array
-	 */
-	public static function get_top_level_templates() {
+            if ($written) {
+                DB::alteration_message(
+                    sprintf('%s error Utility Page created', $code),
+                    'created'
+                );
+            } else {
+                DB::alteration_message(
+                    sprintf(
+                        '%s error Utility page could not be created at %s. Please check permissions',
+                        $code,
+                        $pagePath
+                    ),
+                    'error'
+                );
+            }
+        }
+    }
 
-		$ss_templates_array = array();
-		$current_theme_path = THEMES_PATH . '/' . Config::inst()->get('SSViewer', 'theme');
 
-		//theme directories to search
-		$search_dir_array = array(
-			MAINTENANCE_MODE_PATH .'/templates',
-			$current_theme_path   .'/templates'
-		);
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
 
-		foreach($search_dir_array as $directory) {
+        $fields->addFieldToTab('Root.Main',
 
-			//Get all the SS templates in the directory
-			foreach(glob("{$directory}/*.ss") as $template_path) {
+            $templateDropdownField = DropdownField::create(
+                'RenderingTemplate',
+                _t('MaintenanceMode.RENDERINGTEMPLATE', 'Template to render with'),
+                self::get_top_level_templates()
+            ), 'Content'
 
-				//get the template name from the path excluding the ".ss" extension
-				$template = basename($template_path, '.ss');
+        );
 
-				//Add the key=>value pair to the ss_template_array
-				$ss_templates_array[$template] = $template;
-			}
+        $templateDropdownField->setEmptyString(_t('MaintenanceMode.DEFAULTTEMPLATE', '(Use default template)'));
 
-		}
+        $this->extend('updateCMSFields', $fields);
+        return $fields;
+    }
 
-		return $ss_templates_array;
+    /**
+     * This function returns an array of top-level theme templates
+     * @return array
+     */
+    public static function get_top_level_templates()
+    {
+        $ss_templates_array = array();
+        $current_theme_path = THEMES_PATH . '/' . Config::inst()->get('SSViewer', 'theme');
 
-	}//end get_top_level_templates()
+        //theme directories to search
+        $search_dir_array = array(
+            MAINTENANCE_MODE_PATH .'/templates',
+            $current_theme_path   .'/templates'
+        );
+
+        foreach ($search_dir_array as $directory) {
+
+            //Get all the SS templates in the directory
+            foreach (glob("{$directory}/*.ss") as $template_path) {
+
+                //get the template name from the path excluding the ".ss" extension
+                $template = basename($template_path, '.ss');
+
+                //Add the key=>value pair to the ss_template_array
+                $ss_templates_array[$template] = $template;
+            }
+        }
+
+        return $ss_templates_array;
+    }//end get_top_level_templates()
 }
 
 
@@ -167,33 +169,35 @@ class UtilityPage extends ErrorPage {
  * @author Darren-Lee Joseph <darrenleejoseph@gmail.com>
  * @package maintenancemode
  */
-class UtilityPage_Controller extends Page_Controller {
+class UtilityPage_Controller extends Page_Controller
+{
 
-	private static $url_handlers = array(
-		'*' => 'index'
-	);
+    private static $url_handlers = array(
+        '*' => 'index'
+    );
 
-	private static $allowed_actions = array();
+    private static $allowed_actions = array();
 
-	public function init() {
-		parent::init();
-	}
+    public function init()
+    {
+        parent::init();
+    }
 
-	public function index() {
+    public function index()
+    {
+        $config = $this->SiteConfig();
 
-		$config = $this->SiteConfig();
+        //regular non-admin users should only be able to see this utility page in maintenance mode
+        if (!$config->MaintenanceMode && !Permission::check("ADMIN")) {
+            return $this->redirect(BASE_URL); //redirect to home page
+        }
 
-		//regular non-admin users should only be able to see this utility page in maintenance mode
-		if(!$config->MaintenanceMode && !Permission::check("ADMIN")) {
-			return $this->redirect(BASE_URL); //redirect to home page
-		}
+        $this->response->setStatusCode($this->ErrorCode);
 
-		$this->response->setStatusCode($this->ErrorCode);
+        if ($this->dataRecord->RenderingTemplate) {
+            return $this->renderWith(array($this->dataRecord->RenderingTemplate, 'Page'));
+        }
 
-		if($this->dataRecord->RenderingTemplate) {
-			return $this->renderWith(array($this->dataRecord->RenderingTemplate, 'Page'));
-		}
-
-		return array();
-	}
+        return array();
+    }
 }
